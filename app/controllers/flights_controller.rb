9f577b989221f716c1e1bfd5a2cef171fc3f5a53
ichanmsngkyy <128625300+ichanmsngkyy@users.noreply.github.com>
2025-10-29 00:06:27 +0800

    Finish implementing search and dropdowns

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,13 @@
 class FlightsController < ApplicationController
   def index
-    @flight = Flights.all
+    @departure_options =Flight.all.map do |f |
+      [ "#{f.departure_airport.airport_code}", f.id ]
+    end
+    @arrival_options =Flight.all.map do |f |
+      [ "#{f.arrival_airport.airport_code}", f.id ]
+    end
+    @time_options =Flight.all.map do |f |
+      [ "#{f.start_datetime.strftime("%B %d, %H:%M")}", f.id ]
+    end
   end
 end
